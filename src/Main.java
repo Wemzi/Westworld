6f6e97f4a43da0f250b0d5e8a9d81e94ddd2e8f5
Lukács Dávid István <idu27k@inf.elte.hu>
2021-03-04 13:24:37 +0100

    Merged the branches together

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import View.MainWindow;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Üdv Westworldben!");
+        System.out.println("Üdv Westworldben! :)");
         MainWindow w=new MainWindow();
         w.setVisible(true);
     }
