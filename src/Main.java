852e9d3e1fd43aa6d8e8928bf342209785deb89e
Lukács Dávid István <idu27k@inf.elte.hu>
2021-04-08 06:20:44 +0000

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import View.MainWindow2;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Üdv Westworldben! :)");
+        System.out.println("Udv Westworldben! :)");
         MainWindow2 w=new MainWindow2();
         w.setVisible(true);
     }
